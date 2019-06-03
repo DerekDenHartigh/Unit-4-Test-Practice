35da4c89fc2408a71f1b78b96e9558b365f82302
Derek <derekdenhartigh@gmail.com>
2019-06-03 17:15:27 -0400

    added the server, a little questionable on the router = require(./routing.js) but I think it should work, untested

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const express = require('express');
+
+const router = require("./routing.js");
+const app = express();
+app.use(express.json());
+app.use("/",router);
+app.use(express.static("./public"));
+const port = 3000;
+app.listen(port, ()=> console.log(`listening on port: http://localhost:${port}`));
