4c87fcac7d3c6e9ff7ef4790ea5ef3259035e48a
Derek <derekdenhartigh@gmail.com>
2019-07-13 23:02:43 -0400

    altered server port const

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express(); // creates an express application
 app.use(express.json());  // tells the api to serve up data in json
 app.use("/", router); // indicates what symbol to route with?
 app.use(express.static("./public")); // serves static files from this directory
-const port = 3000; // sets port #
+// const port = 3000; // sets port #
+const port = process.env.PORT || 3000
 app.listen(port, ()=> console.log(`listening on port: http://localhost:${port}`));
 // tells server to listen @ specific port # & makes a link in console
